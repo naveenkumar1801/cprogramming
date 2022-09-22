8a6f6b71ff07f170bbddf0ae8388b7535d141550
Naveen Kumar <113339676+naveenkumar1801@users.noreply.github.com>
2022-09-22 13:32:49 +0530

    Add files via upload

---
+++ h3.cpp
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int main()
+{
+char ch;
+int upper=0;
+int lower=0;
+int digits=0;
+
+do{
+
+printf("\nEnter any character, or * to quit :");
+scanf("%c",&ch);
+ch=getchar();
+
+if (ch>='a'&& ch<='z')
+
+lower++;
+
+if (ch>='A'&&ch<='Z')
+
+upper++;
+
+if (ch>=0)
+
+digits++;
+
+}while(ch!=*);
+
+
+
+printf("\nTotal count of lowercase characters entered = %d",lower);
+
+printf("\nTotal count of uppercase characters entered = %d",upper);
+
+printf("\nTotal count of digits entered = %d",digits);
+
+
+return 0;
+}
