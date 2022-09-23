b5907dbd6c24200c72d25d20243fa398fdc45eb0
Naveen Kumar <113339676+naveenkumar1801@users.noreply.github.com>
2022-09-23 10:06:38 +0530

    Add files via upload

---
+++ Sum of digits.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+int getSum(int n)
+{
+int sum = 0;
+while (n != 0) {
+sum = sum + n % 10;
+n = n / 10;
+}
+return sum;
+}
+int main()
+{
+int n = 87;
+printf(" %d ", getSum(n));
+return 0;
+}
